afb4b85f684c13b7a5ea6614f0ff119250e56e20
fafa1899 <holybell44@126.com>
2026-03-27 15:02:06 +0800

    [A]初始化

---
+++ ndk23/arm64-v8a/sdk/native/jni/OpenCV-arm64-v8a.mk
@@ -0,0 +1,2 @@
+OPENCV_3RDPARTY_COMPONENTS:=libprotobuf libjpeg-turbo libwebp libpng libtiff libjasper IlmImf cpufeatures ittnotify tegra_hal quirc
+OPENCV_EXTRA_COMPONENTS:=D:/Work/Android3rdParty/lib/libz.so dl m log
---
+++ ndk23/arm64-v8a/sdk/native/jni/abi-arm64-v8a/OpenCVModules-release.cmake
@@ -0,0 +1,129 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "libcpufeatures" for configuration "Release"
+set_property(TARGET libcpufeatures APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(libcpufeatures PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "C"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/sdk/native/3rdparty/libs/arm64-v8a/libcpufeatures.a"
+  )
+
+list(APPEND _cmake_import_check_targets libcpufeatures )
+list(APPEND _cmake_import_check_files_for_libcpufeatures "${_IMPORT_PREFIX}/sdk/native/3rdparty/libs/arm64-v8a/libcpufeatures.a" )
+
+# Import target "libjpeg-turbo" for configuration "Release"
+set_property(TARGET libjpeg-turbo APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(libjpeg-turbo PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "C"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/sdk/native/3rdparty/libs/arm64-v8a/liblibjpeg-turbo.a"
+  )
+
+list(APPEND _cmake_import_check_targets libjpeg-turbo )
+list(APPEND _cmake_import_check_files_for_libjpeg-turbo "${_IMPORT_PREFIX}/sdk/native/3rdparty/libs/arm64-v8a/liblibjpeg-turbo.a" )
+
+# Import target "libtiff" for configuration "Release"
+set_property(TARGET libtiff APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(libtiff PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "C;CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/sdk/native/3rdparty/libs/arm64-v8a/liblibtiff.a"
+  )
+
+list(APPEND _cmake_import_check_targets libtiff )
+list(APPEND _cmake_import_check_files_for_libtiff "${_IMPORT_PREFIX}/sdk/native/3rdparty/libs/arm64-v8a/liblibtiff.a" )
+
+# Import target "libwebp" for configuration "Release"
+set_property(TARGET libwebp APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(libwebp PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "C"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/sdk/native/3rdparty/libs/arm64-v8a/liblibwebp.a"
+  )
+
+list(APPEND _cmake_import_check_targets libwebp )
+list(APPEND _cmake_import_check_files_for_libwebp "${_IMPORT_PREFIX}/sdk/native/3rdparty/libs/arm64-v8a/liblibwebp.a" )
+
+# Import target "libjasper" for configuration "Release"
+set_property(TARGET libjasper APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(libjasper PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "C"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/sdk/native/3rdparty/libs/arm64-v8a/liblibjasper.a"
+  )
+
+list(APPEND _cmake_import_check_targets libjasper )
+list(APPEND _cmake_import_check_files_for_libjasper "${_IMPORT_PREFIX}/sdk/native/3rdparty/libs/arm64-v8a/liblibjasper.a" )
+
+# Import target "libpng" for configuration "Release"
+set_property(TARGET libpng APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(libpng PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "C"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/sdk/native/3rdparty/libs/arm64-v8a/liblibpng.a"
+  )
+
+list(APPEND _cmake_import_check_targets libpng )
+list(APPEND _cmake_import_check_files_for_libpng "${_IMPORT_PREFIX}/sdk/native/3rdparty/libs/arm64-v8a/liblibpng.a" )
+
+# Import target "IlmImf" for configuration "Release"
+set_property(TARGET IlmImf APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(IlmImf PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/sdk/native/3rdparty/libs/arm64-v8a/libIlmImf.a"
+  )
+
+list(APPEND _cmake_import_check_targets IlmImf )
+list(APPEND _cmake_import_check_files_for_IlmImf "${_IMPORT_PREFIX}/sdk/native/3rdparty/libs/arm64-v8a/libIlmImf.a" )
+
+# Import target "libprotobuf" for configuration "Release"
+set_property(TARGET libprotobuf APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(libprotobuf PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/sdk/native/3rdparty/libs/arm64-v8a/liblibprotobuf.a"
+  )
+
+list(APPEND _cmake_import_check_targets libprotobuf )
+list(APPEND _cmake_import_check_files_for_libprotobuf "${_IMPORT_PREFIX}/sdk/native/3rdparty/libs/arm64-v8a/liblibprotobuf.a" )
+
+# Import target "quirc" for configuration "Release"
+set_property(TARGET quirc APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(quirc PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "C"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/sdk/native/3rdparty/libs/arm64-v8a/libquirc.a"
+  )
+
+list(APPEND _cmake_import_check_targets quirc )
+list(APPEND _cmake_import_check_files_for_quirc "${_IMPORT_PREFIX}/sdk/native/3rdparty/libs/arm64-v8a/libquirc.a" )
+
+# Import target "tegra_hal" for configuration "Release"
+set_property(TARGET tegra_hal APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(tegra_hal PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/sdk/native/3rdparty/libs/arm64-v8a/libtegra_hal.a"
+  )
+
+list(APPEND _cmake_import_check_targets tegra_hal )
+list(APPEND _cmake_import_check_files_for_tegra_hal "${_IMPORT_PREFIX}/sdk/native/3rdparty/libs/arm64-v8a/libtegra_hal.a" )
+
+# Import target "ittnotify" for configuration "Release"
+set_property(TARGET ittnotify APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(ittnotify PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "C"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/sdk/native/3rdparty/libs/arm64-v8a/libittnotify.a"
+  )
+
+list(APPEND _cmake_import_check_targets ittnotify )
+list(APPEND _cmake_import_check_files_for_ittnotify "${_IMPORT_PREFIX}/sdk/native/3rdparty/libs/arm64-v8a/libittnotify.a" )
+
+# Import target "opencv_world" for configuration "Release"
+set_property(TARGET opencv_world APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(opencv_world PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/sdk/native/staticlibs/arm64-v8a/libopencv_world.a"
+  )
+
+list(APPEND _cmake_import_check_targets opencv_world )
+list(APPEND _cmake_import_check_files_for_opencv_world "${_IMPORT_PREFIX}/sdk/native/staticlibs/arm64-v8a/libopencv_world.a" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
